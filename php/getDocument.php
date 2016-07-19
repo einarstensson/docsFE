04eb140baf133329d82f07ae8c421cb0cae1ec48
einarstensson <einar.stensson@gmail.com>
2016-07-18 22:53:41 -0400

    Remove php sections

--- php/getDocument.php
+++
@@ -1,4 +0,0 @@
-<?php
-    $response = ['id' => 1, 'content' => 'Live long'];
-    echo json_encode($response);
-?>
--- php/saveDocument.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $putRequest = file_get_contents("php://input");
-    parse_str($putRequest, $request);
-
-    echo $request['name'];
-?>
